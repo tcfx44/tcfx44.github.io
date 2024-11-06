52759e9ead666ab6e065ba93426f9679f818ad9c
Chavdar Kirov <chavdar.kirov@gmail.com>
2024-11-07 00:53:53 +0200

    command line to start the tool locally

---
+++ esptool-js/start_esp_tool.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --guest --disable-search-engine-choice-screen --new-window --allow-file-access-from-files --allow-file-access --allow-cross-origin-auth-prompt --user-data-dir="%TEMP%/ESP-TOOL" "%CD%/index.html"
